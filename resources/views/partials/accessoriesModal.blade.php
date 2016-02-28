aafad5582158db34bd3e4aef678bcff3d77889cb
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-02-28 19:42:10 +0000

    collection accessories page

--- resources/views/partials/accessoriesModal.blade.php
+++ resources/views/partials/accessoriesModal.blade.php
@@ -1,8 +1,18 @@
-<div class="modalpage" id="accessories-modal">
-        <div id="closewindow">
-            <div class="closewindow">
-                <a href="#" class="light off-modal">&times;</a>
-            </div>
-        </div>
-        <h2>This is The Accessories modal</h2>
+<div class="modalpage modalpage--white" id="accessories-modal">
+  <div id="closewindow">
+    <div class="closewindow">
+      <a href="#" class="light off-modal">&times;</a>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="col-md-12">
+      <h2>Accessories At Blue Gallery</h2>
+
+      <p class="lead">Kosta Boda Vases</p>
+      {!! (new ThumbSet('/img/furnitureThumbs/kosta_boda_vases/','vertical'))->render()!!}
+
+     </div>
+  </div>
+
+</div>
 </div>
