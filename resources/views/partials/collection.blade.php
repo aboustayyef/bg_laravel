92488420148e9d962fa0e4470ab6179e0189bc00
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-03-12 12:37:56 +0000

    prepare for production

--- resources/views/partials/collection.blade.php
+++ resources/views/partials/collection.blade.php
@@ -6,7 +6,9 @@
 			</div>
 			<div class="col-md-4">
 				<h3 class="bgCollection__h3">Home Furniture</h3>
-				<img class="bgCollection__image lazy" src="/img/collection_ditre.jpg" width="400" height="auto" alt="">
+				<a href="" data-modal="living-room-modal" class="on-modal">
+					<img class="bgCollection__image lazy" src="/img/collection_ditre.jpg" width="400" height="auto" alt="">
+				</a>
 				<p>
 					Modern, Stylish and exclusive. Blue Gallery only stocks the highest standards of sofas, armchairs, coffee tables, Beds and mattresses for the contemporary living room.
 				</p>
@@ -14,7 +16,9 @@
 			</div>
 			<div class="col-md-4">
 				<h3 class="bgCollection__h3">Office Furniture</h3>
-				<img class="bgCollection__image lazy" src="/img/collection_interstuhl.jpg" width="400" height="auto" alt="">
+				<a href="" data-modal="office-furniture-modal" class="on-modal">
+					<img class="bgCollection__image lazy" src="/img/collection_interstuhl.jpg" width="400" height="auto" alt="">
+				</a>
 				<p>
 					High quality office chairs for maximum seating performance, characterised by exceptional  design and state-of-the-art German Technology combined with Italian office furniture that is functional and elegant for a customised and efficient workplace.
 				</p>
@@ -22,7 +26,9 @@
 			</div>
 			<div class="col-md-4">
 				<h3 class="bgCollection__h3">Accessories</h3>
-				<img class="bgCollection__image lazy" src="/img/collection_accessories.jpg" width="400" height="auto" alt="">
+				<a href="" data-modal="accessories-modal" class="on-modal">
+					<img class="bgCollection__image lazy" src="/img/collection_accessories.jpg" width="400" height="auto" alt="">
+				</a>
 				<p>
 					Rugs, Bowls, vases, dishes,shelf objects &amp; candle holders that will make a great complement to any living area with their distinctive and exquisite style to add more life and spirit to your home.
 				</p>
