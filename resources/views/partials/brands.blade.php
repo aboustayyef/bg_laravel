ecd936f0dbca12d85b7469679a81bf11665108ed
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-12-23 07:41:10 +0000

    fixed links in brands to correct websites

--- resources/views/partials/brands.blade.php
+++ resources/views/partials/brands.blade.php
@@ -6,7 +6,7 @@
 				<h2>Our Brands</h2>
 			</div>
 			<div class="col-sm-6">
-				<a href="http://www.ditreitalia.com" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Interstuhl')" >
+				<a href="http://www.interstuhl.com/I/ia-en/index.php" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Interstuhl')" >
 					<div class="brand__poster lazy_bg" data-src="img/brands/interstuhl.jpg"></div>
 				</a>
 			</div>
@@ -20,12 +20,12 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<a href="http://www.ditreitalia.com" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Newform')" >
+				<a href="http://www.newform.it/" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Newform')" >
 					<div class="brand__poster lazy_bg" data-src="img/brands/newform.jpg"></div>
 				</a>
 			</div>
 			<div class="col-sm-6">
-				<a href="http://www.ditreitalia.com" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Kosta Boda')" >
+				<a href="http://kostaboda.com" target="_blank" onclick="ga('send', 'event', 'exit link', 'suppliers website' , 'Kosta Boda')" >
 					<div class="brand__poster lazy_bg" data-src="img/brands/kostaboda.jpg"></div>
 				</a>
 			</div>
