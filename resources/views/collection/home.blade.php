55eb66be93a1911ad593027dace970500364b2ea
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-05-15 06:09:42 +0000

    work in progress

--- resources/views/collection/home.blade.php
+++ resources/views/collection/home.blade.php
@@ -60,31 +60,61 @@ Blue Gallery Home &amp; Office - Collection
 		</div>
 		<hr>
 
-			<h2 class="section__title title">Sofas for the comfort of your family and your guests</h2>
+			<h2 class="section__title section__give_space">Sofas for the comfort of your family and your guests</h2>
 
-			<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_sofas/'))->list ; ?>
-			<div class="card__collection">
-				@foreach ($thumbset as $key => $thumb)
-					@include('collection.card', ['aspect' => 'is-2by1'])
-				@endforeach
+			<div class="columns">
+				<div class="column is-2">
+					<div class="subtitle">
+						A few words to talk about how awesome the sofas are
+					</div>
+				</div>
+				<div class="column">
+					<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_sofas/'))->list ; ?>
+					<div class="card__collection">
+						@foreach ($thumbset as $key => $thumb)
+							@include('collection.card', ['aspect' => 'is-2by1'])
+						@endforeach
+					</div>
+				</div>
 			</div>
 
-			<h2 class="section__title title">Armchairs with Style and Grace</h2>
+			<hr>
 
-			<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_armchairs/'))->list ; ?>
-			<div class="card__collection">
-				@foreach ($thumbset as $key => $thumb)
-					@include('collection.card', ['aspect' => 'is-square'])
-				@endforeach
+			<h2 class="section__title section__give_space has-text-right">Armchairs with Style and Grace</h2>
+			<div class="columns">
+				<div class="column">
+					<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_armchairs/'))->list ; ?>
+					<div class="card__collection">
+						@foreach ($thumbset as $key => $thumb)
+							@include('collection.card', ['aspect' => 'is-square'])
+						@endforeach
+					</div>	
+				</div>
+				<div class="column is-2">
+					<div class="subtitle">
+						A few words to describe the cool armchairs
+					</div>
+				</div>
 			</div>
 
-			<h2 class="section__title title">Beds</h2>
+			<hr>
 
-			<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_beds/'))->list ; ?>
-			<div class="card__collection">
-				@foreach ($thumbset as $key => $thumb)
-					@include('collection.card', ['aspect' => 'is-2by1'])
-				@endforeach
+			<h2 class="section__title section__give_space">Our Beds</h2>
+
+			<div class="columns">
+				<div class="column is-2">
+					<div class="subtitle">
+						A few words to talk about how awesome the beds are
+					</div>
+				</div>
+				<div class="column">
+					<?php $thumbset = (new App\ThumbSet('/img/furnitureThumbs/ditre_beds/'))->list ; ?>
+					<div class="card__collection">
+						@foreach ($thumbset as $key => $thumb)
+							@include('collection.card', ['aspect' => 'is-2by1'])
+						@endforeach
+					</div>
+				</div>
 			</div>
 
 	</div>
